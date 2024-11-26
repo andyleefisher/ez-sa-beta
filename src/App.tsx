4d404aad6e2e935943c4e294f3189971ee725673
Andy Fisher <140590937+andyleefisher@users.noreply.github.com>
2024-11-26 15:55:09 +0000

    Update src/App.tsx [BoltSync]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { InfiniteCanvas } from './components/Canvas/InfiniteCanvas';
+import { Toaster } from '@/components/ui/toaster';
 
 function App() {
   return (
     <div className="h-screen w-screen">
       <InfiniteCanvas />
+      <Toaster />
     </div>
   );
 }
